52173266356c2a3284819b8781de7d98c7c6e744
rm <rm399343@gmail.com>
2017-11-04 02:31:00 +0900

    fix python terminal __template__.py

--- Assets/PythonPrompt/PythonScripts/__template__.py
+++ Assets/PythonPrompt/PythonScripts/__template__.py
@@ -1,10 +1,12 @@
 import clr
 clr.AddReferenceByPartialName('UnityEngine')
 clr.AddReferenceByPartialName('UnityEngine.UI')
+clr.AddReferenceByPartialName('UnityEditor')
+
 from UnityEngine import *
 from UnityEditor import *
 
 import sys
-sys.path.append(Application.dataPath + '/../Lib')
+sys.path.append(Application.dataPath + '/../Plugins')
 
 {0}
