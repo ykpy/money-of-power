cb8fe804144ee2631c103f1525c6e09edfc3b46e
三河 麟太郎 <s1230223@gmail.com>
2017-11-03 21:03:48 +0900

    UIの表示と時間は減少するよう実装

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+	public int time;
+	float times;
+	Text timeText;
+
+	// Use this for initialization
+	void Start () {
+		timeText = this.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		times -= Time.deltaTime;
+		if (times <= 0.0) {
+			times = 1.0f;
+			time--;
+		}
+		timeText.text = time.ToString();
+	}
+}
