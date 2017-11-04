2e8f1b3e5e87d3dbc3f2988f6afa699abcb4f1b2
rm <rm399343@gmail.com>
2017-11-04 20:50:33 +0900

    add damage motion

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -1,8 +1,21 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(PlayerMovement))]
 public class PlayerStatus : MonoBehaviour {
     public int pride;
     public int money;
+
+    PlayerMovement playerMovement;
+
+    private void Start() {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            calculateDamage(0);
+        }
+    }
 
     public void calculateDamage(int damage) {
         if (pride - damage > 0) {
@@ -10,5 +23,7 @@ public class PlayerStatus : MonoBehaviour {
         } else {
             pride = 0;
         }
+
+        playerMovement.Damage();
     }
 }
