80db04eb2f005ff5ea90b926a878adefcabc1276
RyoTatesawa <tate9723@gmail.com>
2017-11-04 19:33:38 +0900

    make_bill

---
+++ Assets/equipment.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class equipment : MonoBehaviour {
+    public Transform hand;
+	// Use this for initialization
+	void Start () {
+        this.gameObject.transform.SetParent(hand);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
