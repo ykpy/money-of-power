a38de450ddc6d4b731ff7f1102d5bdf17a5c490d
rm <rm399343@gmail.com>
2017-11-04 09:23:30 +0900

    add jump SE

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(AudioSource))]
 public class PlayerJump : MonoBehaviour {
 
     Rigidbody rb;
@@ -10,15 +12,17 @@ public class PlayerJump : MonoBehaviour {
     float jumpPower;
     bool isGrounded = true;
 
-    Animator animator;
-
-    CharacterController controller;
     PlayerMovement playerMovement;
+
+    [SerializeField]
+    AudioClip jumpSE;
+    AudioSource audioSource;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
         playerMovement = GetComponent<PlayerMovement>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +31,8 @@ public class PlayerJump : MonoBehaviour {
             if (PlayerInput.GetButtonDown("Jump", playerMovement.PlayerID))
             {
                 rb.velocity = new Vector3(0, jumpPower, 0);
+                // 効果音の再生
+                audioSource.PlayOneShot(jumpSE);
             }
         }
     }
