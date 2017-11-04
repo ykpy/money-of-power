b8036c4203a9f56096fd309c0dd55b31fdbefae3
Boto Mizukoshi <zkzi3254@gmail.com>
2017-11-04 22:11:25 +0900

    add yeeeeeeeeeeeey

--- Assets/Scripts/Managers/ResultManager.cs
+++ Assets/Scripts/Managers/ResultManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResultManager : MonoBehaviour {
+public class ResultManager : MonoBehaviour
+{
 
     PlayerStatus player1Status;
     PlayerStatus player2Status;
@@ -11,11 +12,13 @@ public class ResultManager : MonoBehaviour {
     int time;
     bool isDraw = false;
 
+    public Text winnerName;
     public Text resultText;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         player1Status = GameManager.Instance.player1Status;
         player2Status = GameManager.Instance.player2Status;
         time = TimeManager.Instance.time;
@@ -35,20 +38,22 @@ public class ResultManager : MonoBehaviour {
         // 制限時間に達した場合
         else
         {
-            if(player1Status.money > player2Status.money) {
+            if (player1Status.money > player2Status.money)
+            {
                 winner = player1Status;
             }
-            else if(player1Status.money < player2Status.money)
+            else if (player1Status.money < player2Status.money)
             {
                 winner = player2Status;
             }
             // 所持金が同じ場合プライド残量で判定
             else
             {
-                if(player1Status.pride > player2Status.pride)
+                if (player1Status.pride > player2Status.pride)
                 {
                     winner = player1Status;
-                }else if(player1Status.pride < player2Status.pride)
+                }
+                else if (player1Status.pride < player2Status.pride)
                 {
                     winner = player2Status;
                 }
@@ -58,12 +63,22 @@ public class ResultManager : MonoBehaviour {
                 }
             }
         }
-        resultText.text = winner.gameObject.name;
+        if (isDraw)
+        {
+            resultText.text = "引き分け";
+            winnerName.text = "めずらしい。";
+        }
+        else
+        {
+            resultText.text = "勝者";
+            winnerName.text = winner.gameObject.name;
+        }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
