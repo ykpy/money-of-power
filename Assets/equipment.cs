80c0bfc1de158ea5a5222c5513e2eb185e2c8ece
RyoTatesawa <tate9723@gmail.com>
2017-11-05 14:21:48 +0900

    add_SE

--- Assets/equipment.cs
+++ Assets/equipment.cs
@@ -10,27 +10,49 @@ public class Equipment : MonoBehaviour {
     public int jumpDamage;
     public int pay;
     public int jumpPay;
+    public int loseMoney;
     public PlayerStatus myPs;
     public PlayerMovement playerMovement;
     public PlayerJump pj;
     bool isCollisionDetection = false;
+
+    [SerializeField]
+    AudioClip hitSE;
+    [SerializeField]
+    AudioClip resisterSE;
+    [SerializeField]
+    AudioClip laughSE;
+    AudioSource audioSource;
     // Use this for initialization
     void Start () {
         bc = GetComponent<BoxCollider>();
         bc.enabled = false;
-        this.gameObject.transform.SetParent(hand);
+        this.gameObject.transform.parent.SetParent(hand);
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update () {
         if (PlayerInput.GetButtonDown("Fire1", playerMovement.PlayerID) && bc.enabled == false) {
             bc.enabled = true;
+            Vector3 pos = this.gameObject.transform.localPosition;
+            pos.x *= -2;
+            pos.y *= 11;
+            pos.z *= 7;
+            this.gameObject.transform.localPosition = pos;
+            gameObject.transform.localScale = new Vector3(
+            gameObject.transform.localScale.x * 10,
+            gameObject.transform.localScale.y * 10,
+            gameObject.transform.localScale.z * 10
+                );
+            StartCoroutine(WaitAttack());
         }
 	}
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger Enter");
+       
         if (!other.gameObject.name.StartsWith("Player"))
         {
             return;
@@ -38,9 +60,8 @@ public class Equipment : MonoBehaviour {
 
         if (pj.GetIsGrounded()) {
             other.GetComponent<PlayerStatus>().CalculateDamage(damage);
-            myPs.MinusMoney(pay);
             other.GetComponent<PlayerStatus>().PlusMoney(pay, myPs.money);
-
+            myPs.MinusMoney(pay);
 
         }
         else {
@@ -48,13 +69,39 @@ public class Equipment : MonoBehaviour {
             other.GetComponent<PlayerStatus>().PlusMoney(jumpPay, myPs.money);
             myPs.MinusMoney(jumpPay);
         }
-
-        StartCoroutine(WaitAttack());
+        audioSource.PlayOneShot(hitSE);
+        audioSource.PlayOneShot(resisterSE);
+        bc.enabled = false;
+        Vector3 pos = this.gameObject.transform.localPosition;
+        pos.x /= -2;
+        pos.y /= 11;
+        pos.z /= 7;
+        this.gameObject.transform.localPosition = pos;
+        gameObject.transform.localScale = new Vector3(
+        gameObject.transform.localScale.x / 10,
+        gameObject.transform.localScale.y / 10,
+        gameObject.transform.localScale.z / 10
+            );
     }
 
     IEnumerator WaitAttack()
     {
         yield return new WaitForSeconds(1f);
-        bc.enabled = false;
-   }
+        if (bc.enabled == true) {
+            audioSource.PlayOneShot(laughSE);
+            bc.enabled = false;
+            Vector3 pos = this.gameObject.transform.localPosition;
+            pos.x /= -2;
+            pos.y /= 11;
+            pos.z /= 7;
+            this.gameObject.transform.localPosition = pos;
+            gameObject.transform.localScale = new Vector3(
+            gameObject.transform.localScale.x / 10,
+            gameObject.transform.localScale.y / 10,
+            gameObject.transform.localScale.z / 10
+                );
+            myPs.MinusMoney(loseMoney);
+        }
+    }
+
 }
